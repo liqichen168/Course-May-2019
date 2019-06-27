4f4dec034cd65bb657584387fcbdfe848f76c1b5
achen-temp <achen-temp@nbme.org>
2019-06-27 12:15:49 -0400

    update boot aop project

--- demoboot/coursedemo-boot/src/main/java/com/course/demo/coursedemoboot/config/AopConfig.java
+++ demoboot/coursedemo-boot/src/main/java/com/course/demo/coursedemoboot/config/AopConfig.java
@@ -6,7 +6,6 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
 
 @Configuration
 @Aspect
